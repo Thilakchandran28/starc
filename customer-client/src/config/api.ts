ae0995d021785591fc19a1a08b485f9e7c78b7bc
Revanth <108523682+mikesteel6698@users.noreply.github.com>
2025-06-27 15:44:47 +0530

    Update api.ts

--- customer-client/src/config/api.ts
+++ customer-client/src/config/api.ts
@@ -1,5 +1,5 @@
 // API configuration
-export const API_URL = 'http://localhost:5001';
+export const API_URL = process.env.API_URL;
 
 // Other API-related configuration can be added here
 export const API_TIMEOUT = 30000; // 30 seconds 
