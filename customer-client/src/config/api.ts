b5e6b7aaf54771ec8cd41aaa25e010e93a554813
Revanth <108523682+mikesteel6698@users.noreply.github.com>
2025-06-29 00:42:03 +0530

    Update api.ts

--- customer-client/src/config/api.ts
+++ customer-client/src/config/api.ts
@@ -1,5 +1,5 @@
 // API configuration
-export const API_URL = 'http://localhost:5001';
+export const API_URL = 'https://server.edifai.in';
 
 // Other API-related configuration can be added here
 export const API_TIMEOUT = 30000; // 30 seconds 
